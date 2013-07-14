5c48fe996559821112a0c9d4e1b2906c136cbcb8
Christian Koepp <christian.koepp@tum.de>
2013-07-14 13:23:08 +0200

    Query of accesstoken

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FacebookSearch',
+    version='0.0.1',
+    description='The funniest joke in the world',
+    url='http://github.com/ckoepp/FacebookSearch',
+    author='Flying Circus',
+    author_email='flyingcircus@example.com',
+    license='MIT',
+    packages=[],
+    zip_safe=False)
